ca8fd34585bb3c6173624344a965be9fcc1af80e
Dorukan Canca <dcanca@edu.cdv.pl>
2023-03-16 15:27:22 +0100

    Funny Comment

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Hahahah funny comment
     }
 
     // Update is called once per frame
